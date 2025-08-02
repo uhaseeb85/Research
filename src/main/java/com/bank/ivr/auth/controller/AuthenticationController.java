f7037b513a11da0b0470f54ae175f9a2b92c4a26
JenkinsGenie <jenkins-genie@automated.com>
2025-08-01 17:00:32 -0700

    Fix: Add Missing SLF4J Logger Import (Build #65)

--- src/main/java/com/bank/ivr/auth/controller/AuthenticationController.java
+++ src/main/java/com/bank/ivr/auth/controller/AuthenticationController.java
@@ -1,11 +1,12 @@
 package com.bank.ivr.auth.controller;
 
-
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
+@RequestMapping("/api/auth")
 public class AuthenticationController {
     private static final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
     
-    // Rest of the controller code...
+    // Rest of the controller implementation...
 }
